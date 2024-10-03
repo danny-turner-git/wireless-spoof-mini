02f617851327584f1104722b3bf8042fbedd7de9
danny <daniel.edenturner@gmail.com>
2024-10-03 10:10:44 +0100

    Added simple wifi connection. Wifi events and network interface not started

--- main/wireless-spoof.c
+++ main/wireless-spoof.c
@@ -6,12 +6,24 @@
 #include "esp_event.h"
 
 
+#define SSID "Example SSID"
+#define PASSWORD "Example Password"
+
 //for event loop arguments: .task_name
 #define EVENT_HANDLER_NAME "event_handler"
 
-void event_handler(void* handler_arg, esp_event_base_t base, int32_t id, void* event_data){
+static const char *TAG = "wifi_handler";
+
+static void event_handler(void* handler_arg, esp_event_base_t base, int32_t id, void* event_data){
 
 	//handler logic
+	if (base ==  WIFI_EVENT && id == WIFI_EVENT_STA_START){
+		esp_wifi_connect();
+		ESP_LOGI(TAG,"Connecting to wifi..");
+	}else if (base == WIFI_EVENT && id == WIFI_EVENT_STA_DISCONNECTED){
+		esp_wifi_connect();
+		ESP_LOGI(TAG,"Reconnecting to wifi...");
+	} 
 
 }
 
@@ -34,35 +46,26 @@ void app_main(void)
 	esp_event_handler_register_with(
 		loop_handle,
 		WIFI_EVENT,
-		WIFI_EVENT_STA_START, 
+		WIFI_EVENT_STA_START, //waits for esp_wifi_start() 
                 event_handler, 
 		NULL);
 
-	/*esp_err_t netif = esp_netif_init();
-	if (netif != ESP_OK){
-		ESP_LOGE("%s", "Failed to initialise netword interface...");
-	}
+	//esp_err_t netif = esp_netif_init();
 	
-	wifi_init_config_t wifi_cfg = WIFI_INIT_CONFIG_DEFAULT();
+	wifi_init_config_t wifi_default_cfg = WIFI_INIT_CONFIG_DEFAULT();
+	esp_wifi_init(&wifi_default_cfg);
 
+	wifi_config_t wifi_cfg = {
+		.sta = {
+			.ssid = SSID,
+			.password = PASSWORD,
+			.threshold.authmode = WIFI_AUTH_WPA2_PSK,
+			},
+	};
+	esp_wifi_set_mode(WIFI_MODE_STA);
+	esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_cfg);
 
-	esp_err_t winit_check = esp_wifi_init(&wifi_cfg);
-	if (winit_check != ESP_OK){
-		ESP_LOGE("%s", "Failed to initialise wifi...");
-	}
-
-	
-	esp_err_t mode_check = esp_wifi_set_mode(WIFI_MODE_STA);
-	if (mode_check != ESP_OK){
-		ESP_LOGE("%s", "Failes to set wifi mode...");
-	}
-
-
-
-	while(1){
-		ESP_LOGI("%s","Cycling through loop...");
-		vTaskDelay(5000/ portTICK_PERIOD_MS);
-	}*/
+	esp_wifi_start();
 
 
 
